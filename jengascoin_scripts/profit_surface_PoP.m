d11978354b3179c2cdd4591bd747d1b71de68efe
zackdarpinian <zackd@ksu.edu>
2021-10-15 14:03:50 -0500

    Create profit_surface_PoP.m

---
+++ jengascoin_scripts/profit_surface_PoP.m
@@ -0,0 +1,54 @@
+% This script models the "surface" created by the relationship between hashrate, wallet balance, and profit for a given single wallet miner. This applies to all miners, but larger mining operations will likely use multiple wallets.
+
+% This surface is not a pure Proof-of-Work reward function. This reflects an emerging consensus algorithm tentatively called "Proof of Pledge", which is an amalgamation of many consensus algorithms and a modified reward function. The details will not be explored here, beyond the ante, raffle, and reward functions. Additionally, wallet-shuffling exploitation methods are not yet explored here.
+
+%   The ante (or burn) function is a required deposit to enter the drawing, and follows an inverse proportionality to wallet balance.
+%   The raffle (or lottery) is a true-random sample of all mining addresses which ante the requisite currency to enter the drawing.
+%   The reward function gives logarithmic returns to an address which solves multiple hashes in a given epoch, up to a given limit.
+
+% author: Daru
+
+%% INPUT PARAMETERS %%
+    % Adjust the inputs below:
+burn_coefficient = 1; % enter any positive number. Higher number is a larger ante/burn.
+raffle_win_ratio = 0.2; % enter a positive number no greater than 1. (0 < number <= 1)
+reward_coefficient = 1; % enter any positive number. Higher number scales rewards.
+reward_cutoff = 5; % enter a positive whole number. Max number of accepted hashes in a given epoch.
+
+% Optional adjustments:
+plot_granularity = 101; % Enter a whole number greater than or equal to 10. Determines number of x and y points.
+plot_balance_range_multiplier = 10; % enter any positive number. Scales balance axis.
+plot_extension_percentage = 50; % will accept any number from between -100 and +inf, but is intended to give a natural plot scale, dependent on the reward_cutoff and balance_cutoff. 0, 10, 50, 100 are all reasonable entries.
+
+
+%% INPUT CHECKS %%
+    % Don't touch!
+if (burn_coefficient <= 0) error ("burn_coefficient invalid"); endif
+if (raffle_win_ratio <= 0 || raffle_win_ratio > 1) error("raffle_win_ratio invalid"); endif
+if (reward_coefficient < 0) error ("reward_coefficient invalid"); endif
+if (reward_cutoff < 1 || mod(reward_cutoff,1)) error("reward_cutoff invalid"); endif
+if (plot_granularity < 10 || mod(plot_granularity,1)) error("plot_granularity invalid"); endif
+if (plot_balance_range_multiplier <= 0) error("plot_balance_range_multiplier invalid"); endif
+if (plot_extension_percentage <= -100) error("plot_extension_percentage invalid"); endif
+
+%% INITIALIZE DATA STRUCTURES %%
+    % Don't touch!
+balance_cutoff = sqrt(burn_coefficient); % minimum number of coins in a wallet to mine.
+
+% "x" == "reward_count", which is proportional to hashrate
+% "y" == "wallet_balance", in coins
+% "z" == "profit", in terms of coins
+x_range = [0 reward_cutoff] * plot_extension_percentage;
+x = linspace(x_range(1),x_range(2),plot_granularity);
+
+y_range = [0 balance_cutoff*plot_balance_range_multiplier] * plot_extension_percentage;
+y = linspace(y_range(1),y_range(2),plot_granularity);
+
+
+
+%% MATH %%
+    % Don't touch!
+for index_x = 1:
+
+
+%% PLOT %% 
