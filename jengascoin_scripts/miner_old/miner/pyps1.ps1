8e79c69cdbe35acd8c7bcadba00d5a666874356d
darunoko <darunoko@gmail.com>
2021-11-07 19:35:56 -0600

    y33t

--- jengascoin_scripts/miner_old/miner/pyps1.ps1
+++
@@ -1,2 +0,0 @@
-.\php.exe -c "php.ini" C:\Users\ThinkPad-T440P\Documents\GitHub\py2php\translate_python_to_php C:\Users\ThinkPad-T440P\Documents\GitHub\RandomProjects\jengascoin_scripts\miner_old\eth_stuff.py
-pause
