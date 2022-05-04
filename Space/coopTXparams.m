401cdb0570b7df85cde5791c23349b09fb57b220
darunohito <zdarpzz@gmail.com>
2022-05-03 20:42:02 -0600

    new function

---
+++ Space/coopTXparams.m
@@ -0,0 +1,19 @@
+function radio = coopTXparams(nodes,tgts)
+  c = 299792458;
+  
+  nNodes = length(nodes.xi); nTgts = size(tgts.aer,1);
+  radio.aerVectors = zeros(nNodes,nTgts,3);
+  for ii = 1:nNodes
+    [radio.aerVectors(ii,:,1),radio.aerVectors(ii,:,2),radio.aerVectors(ii,:,3)] = ...
+      geodetic2aer(tgts.geodetic(:,1),tgts.geodetic(:,2),tgts.geodetic(:,3),...
+      nodes.geodetic(ii,1),nodes.geodetic(ii,2),nodes.geodetic(ii,3),'wgs84','degrees');
+  end
+
+  radio.TOF = radio.aerVectors(:,:,3) / c; %seconds, time of flight to target
+  radio.offsets = zeros(1,nTgts); nodes.delays = zeros(nNodes,nTgts);
+  for ii = 1:nTgts
+    radio.offsets(ii) = max(radio.TOF(:,ii)); %timing offsets based on max TOF
+    radio.delays(:,ii) = radio.offsets(ii) - radio.TOF(:,ii); %delays from trigger point until TX start
+  end
+
+endfunction
--- Space/worldread_script.m
+++ Space/worldread_script.m
@@ -1,13 +1,13 @@
 % pkg load mapping
 tic
 close all
-nNodes = 1; nTgts = 1;
-worldFileName = [pwd '\ETOPO1_Ice_c_geotiff\ETOPO1_Ice_c_geotiff.tif'];
+
+nNodes = 4; nTgts = 2; fCenter = 100e6; 
+worldFileName = [pwd '\ETOPO1_Ice_g_geotiff\ETOPO1_Ice_g_geotiff.tif'];
 
 locationWindow = [-107.34, 34.48; -106.27, 35.39]; % long/lat deg, ABQ area
 tgtMinElevationDeg = 30; % deg from vertical
 tgtRangeWindow= [5e3, 15e3]; % ([min, max] from plot center)
-pointingError = 5; % degrees of average TX pointing error
 
 %% read file
 if ~exist('bands') || ~strcmp(bands.name,worldFileName)
@@ -17,14 +17,14 @@ end
 
 [nodes, h] = generateNodes(bands,info,locationWindow,nNodes,'plot');
 [tgts, h] = generateTargets(tgtMinElevationDeg,tgtRangeWindow,nodes.center,nTgts,'plot',h); 
+radio = coopTXparams(nodes, tgts);
 
-nodes.aerTargets = zeros(nNodes,nTgts,3);
-for ii = 1:nNodes
-  [nodes.aerTargets(ii,:,1),nodes.aerTargets(ii,:,2),nodes.aerTargets(ii,:,3)] = ...
-    geodetic2aer(tgts.geodetic(:,1),tgts.geodetic(:,2),tgts.geodetic(:,3),...
-    nodes.geodetic(ii,1),nodes.geodetic(ii,2),nodes.geodetic(ii,3),'wgs84','degrees');
-end
+% should I solve the analytical solutions for each radiator at each point in space?
+% plot power density in free space, with superimposed beams?
+% should I do the same work with FSPL, time delay, and a real signal/reflection?
+
+% maybe I should focus on receive & autocorrelation first?
 
 
-  
+
 toc
